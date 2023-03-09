686b2c7c7cf14bd47c8a3314cbb440cf5b0473ca
Liren21 <kuminovvv21@gmail.com>
2023-03-10 00:04:39 +0600

    upd

--- app/src/main/java/com/zaim/microzaim/MainActivity.kt
+++ app/src/main/java/com/zaim/microzaim/MainActivity.kt
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
-    private val URL = "https://storageru.github.io/#/"
+    private val URL = "https://microzaim.github.io/#/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
